8e07b2ce36115f9d13097c3492d87d6e8a387e6e
pascal-adp <pascal.alonso.de.prado@rwth-aachen.de>
2023-10-25 20:30:21 +0200

    Added one of the design colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        "ws-claret": "#750D37"
+      }
     },
   },
   plugins: [],
