5ec4cbfda3226aad33566ddbf22d7c29c47f74b3
pascal-adp <pascal.alonso.de.prado@rwth-aachen.de>
2023-10-25 21:17:27 +0200

    Updated Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <nav className="ml-5 flex h-16 w-screen items-center border-b-2">
-      <p className="text-3xl font-bold tracking-[-0.1em] text-ws-claret">
+    <nav className="flex h-16 w-screen items-center border-b-2">
+      <p className="ml-5 text-3xl font-bold tracking-[-0.1em] text-ws-claret">
         weeklysharelist!
       </p>
     </nav>
